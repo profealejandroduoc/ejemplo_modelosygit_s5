ee6211c1f0ddd83f5f5317ba764df84d784b6d56
El profe <al.sepulvedam@profesor.duoc.cl>
2023-05-19 09:50:32 -0400

    Modelo Persona Agregado

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+SEXO = [
+    ("F", "Femenino"),
+    ("M", "Masculino"),
+    ("O", "Otro"),]
+
+class Persona(models.Model):
+    rut=models.CharField(primary_key=True,max_length=10)
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    f_nacimiento=models.DateField()
+    sexo=models.CharField(max_length=1,choices=SEXO)
