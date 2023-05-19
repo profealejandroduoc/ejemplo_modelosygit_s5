87de88d3ba097d59df224d10b643ec3d1d07164c
El profe <al.sepulvedam@profesor.duoc.cl>
2023-05-19 10:29:37 -0400

    modelo mascota y persona agregados

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Persona, Mascota
 # Register your models here.
+
+class admPersona(admin.ModelAdmin):
+    list_display=["rut","nombre","apellido","f_nacimiento","sexo"]
+    list_editable=["nombre","apellido","f_nacimiento","sexo"]
+
+    class meta:
+        model=Persona
+
+class admMascota(admin.ModelAdmin):
+    list_display=["id","nombre","tipo","persona"]
+    list_editable=["nombre","tipo","persona"]
+
+
+    class meta:
+        model=Mascota
+
+
+admin.site.register(Persona,admPersona)
+admin.site.register(Mascota,admMascota)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -13,3 +13,16 @@ class Persona(models.Model):
     apellido=models.CharField(max_length=50)
     f_nacimiento=models.DateField()
     sexo=models.CharField(max_length=1,choices=SEXO)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
+
+class Mascota(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    tipo=models.CharField(max_length=50)
+    persona=models.ForeignKey("Persona",on_delete=models.PROTECT)
+
+    def __str__(self):
+            return self.nombre
